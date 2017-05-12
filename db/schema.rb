573e274e80ee9ceed42c65ce029a36dad29c8c7b
Tanner Eustice <teustice@msn.com>
2017-05-12 09:35:43 -0700

    fix join table shoes_stores

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170512160923) do
+ActiveRecord::Schema.define(version: 20170512163339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 20170512160923) do
   create_table "shoes", force: :cascade do |t|
     t.string "brand"
     t.integer "price"
+  end
+
+  create_table "shoes_stores", force: :cascade do |t|
+    t.integer "shoe_id"
+    t.integer "store_id"
   end
 
   create_table "stores", force: :cascade do |t|
