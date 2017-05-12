bb1480f687dcc54b539dc3614d85d9126d6dcf48
Tanner Eustice <teustice@msn.com>
2017-05-12 10:02:51 -0700

    add form to create new stores

--- app.rb
+++ app.rb
@@ -6,3 +6,14 @@ get "/" do
   @stores = Store.all
   erb :index
 end
+
+#ADD STORE
+get "/store/new" do
+  erb :store_new
+end
+
+post "/store/new" do
+  name = params['name']
+  Store.find_or_create_by(name: name)
+  redirect "/"
+end
--- spec/shoestore_integration_spec.rb
+++ spec/shoestore_integration_spec.rb
@@ -1,5 +1,14 @@
 require "spec_helper"
 
+describe("the /store/new path", {:type => :feature}) do
+  it("creates a new store and verify's it's presence on the '/' path") do
+    visit("/")
+    click_link("Add a store")
+    fill_in("name", :with => "Nike")
+    click_button("Create")
+    expect(page).to have_content("Nike")
+  end
+end
 # describe("the phrase parser path", {:type => :feature}) do
 #   it("processes the user input and returns correct message if its a palindrome") do
 #     visit("/")
